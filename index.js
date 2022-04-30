251ac39e9ced962b37c0126a553b7c0bbfcb99fb
Anonim230 <73050403+Anonim230@users.noreply.github.com>
2022-04-30 13:11:25 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require('express')
 const router = require('./routes')
-const http = require('http');
 require('./model/model')
 const app = express()
 app.use(express.json())
 const port = process.env.PORT || 3000
 // console.log(http.STATUS_CODES);
 app.use(router)
-
 app.listen(port, () => console.log(`Listening on port ${port}`))
 module.exports = app
