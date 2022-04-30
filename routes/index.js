f5532246c5700d404b4bac93238f35c54ccb8896
Anonim230 <73050403+Anonim230@users.noreply.github.com>
2022-04-30 13:25:40 +0500

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,8 @@ const router = Router()
 const imports = { matras, user }
 for (let i of Object.keys(imports)) {
     for (let j of Object.keys(imports[i]))
-        if (j === 'delete') router.delete(`/${i}`, imports[i][j])
-        else j === 'get' ? router.get(`/${i}`, imports[i][j]) : router.post(`/${i}`, imports[i][j])
+        if (j === 'delete') router.delete(`/api/${i}`, imports[i][j])
+        else j === 'get' ? router.get(`/api/${i}`, imports[i][j]) : router.post(`/api/${i}`, imports[i][j])
 }
 
 module.exports = router
